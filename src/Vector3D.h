43dd6afa08ff08e7c63cf056354045919b0cf8e1
zdrvzbl <sanya2003ogorodov@gmail.com>
2023-10-03 21:22:59 +0700

    refactor: constructor by value

--- src/Vector3D.h
+++ src/Vector3D.h
@@ -8,7 +8,7 @@ struct Vector3D
     T z;
 
     vector3D() : x(0), y(0), z(0) {}
-    vector3D(const T& X, const T& Y, const T& Z) : x(X), y(Y), z(Z) {}
+    vector3D(T x, T y, T z) : x(x), y(y), z(y) {}
     vector3D(const vector3D&) = default;
     vector3D(vector3D&&) noexcept = default;
     vector3D& operator=(const vector3D&) = default;
