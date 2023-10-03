858575b213004b6ed0fb6df0bb34250ea5413d81
zdrvzbl <sanya2003ogorodov@gmail.com>
2023-10-03 22:14:44 +0700

    refactor: hardcode float in curve class

--- src/ICurve.h
+++ src/ICurve.h
@@ -4,12 +4,11 @@
 class ICurve
 {
 public:
-    using ParamTy = float;
     using PointTy = Vector3D<float>;
 
 public:
     virtual ~ICurve() = default;
 
-    virtual PointTy Point(ParamTy t) = 0;
-    virtual PointTy Derivative(ParamTy t) = 0;
+    virtual PointTy Point(float t) const = 0;
+    virtual PointTy Derivative(float t) const = 0;
 };
