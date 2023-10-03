5b664cddfff799a124fd10c1a0e7a040b7d26ce1
zdrvzbl <sanya2003ogorodov@gmail.com>
2023-10-03 20:48:08 +0700

    add curve interface

---
+++ src/ICurve.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Vector3D.h"
+
+class ICurve
+{
+public:
+    using ParamTy = float;
+    using PointTy = Vector3D<float>;
+
+public:
+    virtual ~ICurve() = default;
+
+    virtual PointTy Point(ParamTy t) = 0;
+    virtual ParamTy Derivative(ParamTy t) = 0;
+};
