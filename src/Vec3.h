fc8344dd83bc4906b4e82f44fb4101b11a4ee54a
zdrvzbl <sanya2003ogorodov@gmail.com>
2023-10-05 18:59:30 +0700

    refactor: remove extra defs

--- src/Vec3.h
+++ src/Vec3.h
@@ -3,18 +3,12 @@
 template<class T>
 struct Vec3
 {
-    T x;
-    T y;
-    T z;
+    T x{};
+    T y{};
+    T z{};
 
-    Vec3() : x(0), y(0), z(0) {}
+    Vec3() = default;
     explicit Vec3(T x, T y, T z) : x(x), y(y), z(z) {}
-    Vec3(const Vec3&) = default;
-    Vec3(Vec3&&) noexcept = default;
-    Vec3& operator=(const Vec3&) = default;
-    Vec3& operator=(Vec3&&) noexcept = default;
-
-    ~Vec3() = default;
 
     bool operator==(const Vec3&) const = default;
 };
