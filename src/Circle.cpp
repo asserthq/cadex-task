57ffe79aec8e32ef8c255531bf3f84b7b4cda564
zdrvzbl <sanya2003ogorodov@gmail.com>
2023-10-05 17:55:00 +0700

    fix: c++ trig funcs (std)

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -3,10 +3,10 @@
 
 Vec3f Circle::Point(float t) const
 {
-    return Vec3f{ m_rad * cos(t), m_rad * sin(t), 0.f };
+    return Vec3f{ m_rad * std::cos(t), m_rad * std::sin(t), 0.f };
 }
 
 Vec3f Circle::Derivative(float t) const
 {
-    return Vec3f{ -m_rad * sin(t), m_rad * cos(t), 0.f };
+    return Vec3f{ -m_rad * std::sin(t), m_rad * std::cos(t), 0.f };
 }
