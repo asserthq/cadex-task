9659eeb9cce0669205c7bfa1dbac98a32a895d87
zdrvzbl <sanya2003ogorodov@gmail.com>
2023-10-05 19:20:07 +0700

    refactor: circle default rad = 1

--- src/Circle.h
+++ src/Circle.h
@@ -6,6 +6,7 @@
 class Circle : public ICurve
 {
 public:
+    Circle() = default;
     explicit Circle(float radius) : m_rad(std::abs(radius)) {}
 
     virtual Vec3f Point(float t) const override;
@@ -17,5 +18,5 @@ public:
     bool operator==(const Circle& other) const { return m_rad == other.m_rad; };
 
 protected:
-    float m_rad;
+    float m_rad{1.f};
 };
