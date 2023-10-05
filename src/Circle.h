369d144d06ce69580eb0627212a81f5d9c96fb66
zdrvzbl <sanya2003ogorodov@gmail.com>
2023-10-05 18:58:10 +0700

    add: get/set, ==, invariant positive radius

--- src/Circle.h
+++ src/Circle.h
@@ -1,16 +1,21 @@
 #pragma once
 #include "ICurve.h"
 
-#include <algorithm>
+#include <cmath>
 
 class Circle : public ICurve
 {
 public:
-    explicit Circle(float radius) : m_rad(std::max(radius, 0.f)) {}
+    explicit Circle(float radius) : m_rad(std::abs(radius)) {}
 
     virtual Vec3f Point(float t) const override;
     virtual Vec3f Derivative(float t) const override;
 
-private:
+    float Radius() const { return m_rad; }
+    void SetRadius(float radius) { m_rad = std::abs(radius); }
+
+    bool operator==(const Circle& other) const { return m_rad == other.m_rad; };
+
+protected:
     float m_rad;
 };
