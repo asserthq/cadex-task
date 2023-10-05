18398f33ab09e5fe0fba5eccf43098ec1f217d26
zdrvzbl <sanya2003ogorodov@gmail.com>
2023-10-05 19:22:18 +0700

    style: remove long line

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -3,10 +3,18 @@
 
 Vec3f Circle::Point(float t) const
 {
-    return Vec3f{ m_rad * std::cos(t), m_rad * std::sin(t), 0.f };
+    return Vec3f{ 
+        m_rad * std::cos(t), 
+        m_rad * std::sin(t), 
+        0.f 
+    };
 }
 
 Vec3f Circle::Derivative(float t) const
 {
-    return Vec3f{ -m_rad * std::sin(t), m_rad * std::cos(t), 0.f };
+    return Vec3f{ 
+        -m_rad * std::sin(t), 
+        m_rad * std::cos(t), 
+        0.f 
+    };
 }
