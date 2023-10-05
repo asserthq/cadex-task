bb7e8d3f5da0be9c606c77560bbdb00c20ccadbf
zdrvzbl <sanya2003ogorodov@gmail.com>
2023-10-05 20:28:16 +0700

    fix: helix formula

--- src/Helix.cpp
+++ src/Helix.cpp
@@ -1,11 +1,14 @@
 #include "Helix.h"
+#include <numbers>
+
+constexpr auto pi = std::numbers::pi_v<float>;
 
 Vec3f Helix::Point(float t) const
 {
     return Vec3f{ 
         m_rad * std::cos(t), 
         m_rad * std::sin(t), 
-        m_step * t
+        m_step * (t / (2 * pi))
     };
 }
 
@@ -14,6 +17,6 @@ Vec3f Helix::Derivative(float t) const
     return Vec3f{ 
         -m_rad * std::sin(t), 
         m_rad * std::cos(t), 
-        m_step
+        m_step / (2 * pi)
     };
 }
